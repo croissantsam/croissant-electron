ce2fa02d96d060c76e44efe8131a25338a8bedb8
Sam <samuel.s.mamane@gmail.com>
2026-08-18 23:21:16 +0200

    remove link

--- src/renderer/src/routes/__root.tsx
+++ src/renderer/src/routes/__root.tsx
@@ -1,6 +1,6 @@
 import "../styles/app.css";
 
-import { createRootRoute, Link, Outlet } from "@tanstack/react-router";
+import { createRootRoute, Outlet } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
 
 const RootLayout = () => (
